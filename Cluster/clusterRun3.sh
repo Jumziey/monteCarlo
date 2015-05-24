9a975a9ceb1659af471f843f10c5a653ce815f8d
Jesper Vesterberg <jeve0010@sesam.(none)>
2015-05-24 15:39:43 +0200

    new clusterRun size

--- Cluster/clusterRun3.sh
+++ Cluster/clusterRun3.sh
@@ -3,7 +3,7 @@
 declare -a temp=("2.20" "2.22" "2.24" "2.25" "2.255" "2.26" "2.262" "2.624" "2.266" "2.268" "2.269")
 L=64
 nblock=64
-nsamp=10000
+nsamp=100000
 
 
 commando="./ising L=$L nblock=$nblock nsamp=$nsamp"
