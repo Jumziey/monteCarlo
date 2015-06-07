a5c48ac4ed68a58156ce1116ecca625927b01497
Jesper Vesterberg <jumziey@gmail.com>
2015-06-07 10:51:21 +0200

    fixed binders run parameters

--- runs/bindersRun.sh
+++ runs/bindersRun.sh
@@ -1,19 +1,21 @@
 
 
 cd ../Cluster
-declare -a temp=("2.31" "2.285" "2.275")
+declare -a temp=("2.20" "2.22" "2.24" "2.25" "2.26" "2.27" "2.275" "2.28" "2.285" "2.29" "2.30" "2.31" "2.32" "2.34" "2.36" "2.38" "2.40" "2.44" "2.48" "2.52" "2.56" "2.60" "2.70")
 declare -a size=("16" "32" "64" "128" "256")
-nblock=64
+
 nsamp=1000;
 
+commando="./ising"
 for L in "${size[@]}"
 do
 	for T in "${temp[@]}"
 	do
-		commando="./ising L=$L nblock=$nblock nsamp=$nsamp T=$T run"
-		echo $commando
-		eval $commando
+		commando="$commando L=$L binders=1 tcorr=1 nblock=$nblock nsamp=$nsamp T=$T run"
 	done
 done
+echo $commando
+sleep 5
+eval $commando
 cd ../runs
 
