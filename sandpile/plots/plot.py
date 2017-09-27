2c58d62f95d7e094e6bddd72eb0f70983d50e4d0
Jesper Vesterberg <jumziey@gmail.com>
2017-09-27 14:11:27 +0200

    Removed style func (not used) and return hs instead of plot handle

--- sandpile/plots/plot.py
+++ sandpile/plots/plot.py
@@ -22,10 +22,10 @@ def Avalanche(typeof, length, cutoff, binsize, marker, fig):
     nrofbins = np.ceil(hs.max()/binsize)
     print("###########################");
     print("Length: "+ str(length))
-    print("Actual nrofbins: " + str(hs.max()/binsize))
-    print("hs.max():" + str(hs.max()))
     print("nrofbins: " + str(nrofbins))
-    print("binsize: " + str(hs.max()/nrofbins))
+    print("Actual nrofbins: " + str(hs.max()/binsize))
+    print("binsize: " + str(binsize))
+    print("Actual binsize: " + str(hs.max()/nrofbins))
     print("###########################");
     hs = hs.value_counts(bins=nrofbins)
     
@@ -35,20 +35,9 @@ def Avalanche(typeof, length, cutoff, binsize, marker, fig):
     #Balancing histogram to the "middle value"
     hs.index = hs.index + binsize/2
 
-    return plt.loglog(hs, marker)
+    plt.loglog(hs, marker)
+    return hs
     
-
-def styleAvalanche():
-    plt.style.use('fivethirtyeight')
-
-    plt.ylabel("Number of occurances")
-    plt.xlabel("Avalanche "+typeof)
-    plt.title("Sandpile with length L="+str(length)+"\n Avalanche "+typeof)
-
-    plt.tight_layout()
-    plt.ylim(ymin=1)
-    plt.xlim(1,np.power(10,5))
-
 def loadAvalancheData(length):
     filename = datadir + "L" + str(length) + "a1000000"
     return pd.read_csv(filename, delimiter='\t')
